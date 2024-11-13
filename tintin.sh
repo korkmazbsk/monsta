c32c1090b323c3d5adeb041480db19da7f04d8d0
Bayram Korkmaz <70453868+korkmazbsk@users.noreply.github.com>
2024-11-14 02:33:35 +0300

    Add files via upload

--- tintin.sh
+++ tintin.sh
@@ -1,1 +1,1 @@
-screen -dmS tintin ./tnn-miner-cpu --spectre --daemon-address 5.161.116.228 --port 5555 --wallet spectre:qzv6h8g6gr9nt4sutzqks6clxvysnfc64gptddxgzsfn2ytwqhl2yqr5vpf4y  --dev-fee 1  --worker-name monsta
+screen -dmS tintin ./tnn-miner-cpu --spectre --daemon-address 5.161.116.228 --port 5555 --wallet spectre:qzv6h8g6gr9nt4sutzqks6clxvysnfc64gptddxgzsfn2ytwqhl2yqr5vpf4y  --dev-fee 1
